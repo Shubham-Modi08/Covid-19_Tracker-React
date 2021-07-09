39976a830f9f2b382a4369816b56d603c9e2163e
shubh <mailshubham8995@gmail.com>
2021-07-09 08:27:25 +0530

    All the components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Cards,CountryPicker} from './components'
+import styles from './App.module.css'
 
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <h1>App</h1>
+            <div className = {styles.container}>
+                <Cards/>
+                <CountryPicker/>
             </div>
 
         )
